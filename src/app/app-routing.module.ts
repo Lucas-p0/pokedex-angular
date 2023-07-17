3915b54d9b47aa1947fe75ec3b7b43c35d176f78
Lucas pinheiro <lucaslogos@gmail.com>
2023-07-17 16:32:23 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(p => p.PagesModule)
+  }
+];
 
 @NgModule({
   declarations: [],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
 
 
 
 
 @NgModule({
     declarations: [
-    HomeComponent
-  ],
+        HomeComponent
+    ],
     imports: [
-        CommonModule
+        CommonModule,
+        RoutingModule
     ]
 })
 export class PagesModule {
