c2e11d224d70d938aa58c464ff95969e9f4aa9ab
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2023-07-19 23:40:42 -0300

    update

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -16,7 +16,8 @@ import { SharedModule } from '../shared/shared.module';
     imports: [
         CommonModule,
         RoutingModule,
-        SharedModule
+        SharedModule,
+
     ]
 })
 export class PagesModule { }
