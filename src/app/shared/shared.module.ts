e6ad6ec833cd0af2282d54332a77e17743cd25da
Lucas pinheiro <lucaslogos@gmail.com>
2023-07-21 17:45:04 -0300

    update

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
-    PokeSearchComponent
+    PokeSearchComponent,
+    PokeListComponent
   ],
   exports: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
   ],
   imports: [
     CommonModule
